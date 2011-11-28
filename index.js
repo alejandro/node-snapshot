c2ca7feb7d942f90bf7c9b733ddbe23e5161e899
Alejandro Morales <vamg008@gmail.com>
2011-11-28 17:23:45 -0600

    [minor] better error handlers and validation, TODO-> headers

--- index.js
+++ index.js
@@ -7,10 +7,16 @@ app.configure(function(){
   app.use(express.bodyParser());
 });
 app.get('/',function(req,res){
-  var  url = 'http://' + req.query.url;
-  request(url, function(e,d,b){
-    if (!e){
+  var  url = 'http://' + req.query.url,
+  options= {uri:url, headers:req.headers};
+if (/^(http:\/\/)([\w]+\.){1,}[A-Z]{2,}\b/gi.test(url)){
+ request(url, function(e,d,b){
+    if (b != undefined && b != null){
+    b = new Buffer(b);b = b.toString(); var n = 0;
       jsdom.env({html: b,scripts: []}, function (err, window) {
+        if (err){
+          res.end('["ERROR","Internal server error"]');
+        }else{
         var imgs =[], document = window.document;
         for(var i in document.images){
           if (document.images[i].src != undefined){
@@ -21,16 +27,29 @@ app.get('/',function(req,res){
                imgs.push(url +(document.images[i].src.substr(0,1)==='/' ? document.images[i].src : '/'+ document.images[i].src ));
              }
            }
-           if (i === 'length'){
+           if (Object.keys(document.images).length === n) {
              res.writeHeader(200,{'Content-Type':'application/json'})
              res.end(JSON.stringify(imgs));
            }
+           n++;
+        }
         }
      });
+      
     } else {
      res.writeHead(404);
-     res.end(' No found  ');
+     res.end('["No found: Use\n http://snap.nodejitsu.com/?url=URL"]');
     }
   });
+} else {
+  res.writeHead(200);
+  res.end('["Invalid URL"]');
+}
+
+ });
+ process.on('uncaughtException', function(excp) {
+  console.log(excp.message);
+  console.info((excp.stack).grey);
+
 });
 app.listen(9000);
